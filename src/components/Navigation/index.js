72d1f259300177e2c71a9b25d8feb0f343b9b280
Reinoptland <reinoptland@gmail.com>
2020-03-20 16:04:02 +0100

    Component, Route and navigation for the product list

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -22,7 +22,7 @@ export default function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
           <NavbarItem path="/" linkText="Home" />
-          <NavbarItem path="/other" linkText="Other" />
+          <NavbarItem path="/products" linkText="Buy stuff" />
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>

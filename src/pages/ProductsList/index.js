8961f85b635ce04472eb955332044d19502fc255
Reinoptland <reinoptland@gmail.com>
2020-03-20 16:22:07 +0100

    Display products

--- src/pages/ProductsList/index.js
+++ src/pages/ProductsList/index.js
@@ -1,13 +1,33 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getProductsThunk } from "../../store/products/actions";
+import { selectProducts } from "../../store/products/selectors";
 
 export default function ProductsList() {
   const dispatch = useDispatch();
+  const products = useSelector(selectProducts);
+
+  console.log(products);
 
   useEffect(() => {
     dispatch(getProductsThunk());
   }, [dispatch]);
 
-  return <div>PRODUCTLIST</div>;
+  return (
+    <div>
+      PRODUCTLIST
+      <div>
+        {products.map(product => {
+          return (
+            <div key={product.id}>
+              <h2>{product.name}</h2>
+              <h2>PRICE €{product.price}</h2>
+              <img src={product.imageUrl} />
+              <p>{product.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }

999cafa4c6d69d9d374c11984710dc78c97acd0f
Reinoptland <reinoptland@gmail.com>
2020-03-20 17:58:08 +0100

    Give feedback, update state

--- src/store/products/actions.js
+++ src/store/products/actions.js
@@ -1,5 +1,6 @@
 import { apiUrl } from "../../config/constants";
 import axios from "axios";
+import { setMessage } from "../appState/actions";
 
 export const getProductsThunk = () => {
   return async (dispatch, getState) => {
@@ -12,6 +13,28 @@ export const getProductsThunk = () => {
 
 export const postProductThunk = (name, description, price, imageUrl) => {
   return async (dispatch, getState) => {
+    const state = getState();
+    const token = state.user.token;
+    console.log(token);
     console.log(name, description, price, imageUrl);
+    // axios post
+    const response = await axios.post(
+      `${apiUrl}/products`,
+      {
+        name: name,
+        description: description,
+        price,
+        imageUrl
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+
+    console.log("RESPONSE", response);
+    dispatch(setMessage("success", true, response.data.message));
+    dispatch({ type: "PRODUCT_ADDED", payload: response.data.product });
   };
 };

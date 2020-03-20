7b43b36f9ddc837a15355361d60fa13cfa352f61
Reinoptland <reinoptland@gmail.com>
2020-03-20 16:10:12 +0100

    Simple thunk used in products list

--- src/pages/ProductsList/index.js
+++ src/pages/ProductsList/index.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getProductsThunk } from "../../store/products/actions";
 
 export default function ProductsList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProductsThunk());
+  }, [dispatch]);
+
   return <div>PRODUCTLIST</div>;
 }
---
+++ src/store/products/actions.js
@@ -0,0 +1,5 @@
+export const getProductsThunk = () => {
+  return async (dispatch, getState) => {
+    console.log("hi");
+  };
+};

f77863c4370a51c88703d825579cd45099924d5c
Reinoptland <reinoptland@gmail.com>
2020-03-20 16:14:32 +0100

    Fetch products, dispatch action on success

--- src/store/products/actions.js
+++ src/store/products/actions.js
@@ -1,5 +1,11 @@
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
+
 export const getProductsThunk = () => {
   return async (dispatch, getState) => {
-    console.log("hi");
+    const response = await axios.get(`${apiUrl}/products`);
+
+    console.log(response.data);
+    dispatch({ type: "FETCH_PRODUCTS_SUCCESS", payload: response.data });
   };
 };
